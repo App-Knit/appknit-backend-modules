9571ab879989e9d3309b2d8719e19afe8c148b84
gaurav <sharma02gaurav@gmail.com>
2018-10-03 12:26:16 +0530

    updating processing build

--- index.js
+++ index.js
@@ -6,13 +6,5 @@
  * @since 28th september 2018
  * @license AppKnit
  */
-
 export * from './utility';
 export * from './services';
-// const utility = require('./utility');
-// const services = require('./services');
-
-// module.exports = {
-// 	Utilities: utility,
-// 	Services: services,
-// };
