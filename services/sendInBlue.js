cba30e9ec6951b54631c0d3f1487788101138cea
jaggi47 <mr.ranveer7@gmail.com>
2020-07-22 02:08:05 +0530

    ABB-2: review changes

--- services/sendInBlue.js
+++ services/sendInBlue.js
@@ -8,23 +8,23 @@ import { ResponseUtility } from '../utility';
 
 const { SEND_IN_BLUE_EMAIL, SEND_IN_BLUE_PASSWORD } = process.env;
 
-const transporter = nodemailer.createTransport({
-	host: 'smtp-relay.sendinblue.com',
-	port: 587,
-	auth: {
-		user: SEND_IN_BLUE_EMAIL,
-		pass: SEND_IN_BLUE_PASSWORD,
-	},
-});
-
 /**
  * function to send mail
- * @param {String} to		-> send email to
- * @param {String} text		-> email content
- * @param {String} subject	-> subject of email
- * @param {html} html		-> html or plain email content
+ * @param {String} to send email to
+ * @param {String} text email content
+ * @param {String} subject subject of email
+ * @param {html} html html or plain email content
  */
 export default ({ to, html, subject }) => new Promise((resolve, reject) => {
+	const transporter = nodemailer.createTransport({
+		host: 'smtp-relay.sendinblue.com',
+		port: 587,
+		auth: {
+			user: SEND_IN_BLUE_EMAIL,
+			pass: SEND_IN_BLUE_PASSWORD,
+		},
+	});
+
 	transporter.sendMail({
 		from: SEND_IN_BLUE_EMAIL,
 		to,
@@ -32,7 +32,7 @@ export default ({ to, html, subject }) => new Promise((resolve, reject) => {
 		subject,
 	}, (err) => {
 		if (err) {
-			return reject(ResponseUtility.GENERIC_ERR({ message: 'Error sending email.', error: err }));
+			return reject(ResponseUtility.GENERIC_ERR({ message: err.message, error: err }));
 		}
 		return resolve(ResponseUtility.SUCCESS);
 	});
